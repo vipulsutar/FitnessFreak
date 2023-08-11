29f879af09aa7adb81b8165d03f9cdab551e48f9
vipulsutar <sutarvipul675@gmail.com>
2023-08-11 22:48:53 +0530

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': '00cca78c4cmsh529ff41e76f2ad2p1ac050jsn31bf988ba608',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '00cca78c4cmsh529ff41e76f2ad2p1ac050jsn31bf988ba608',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  return data;
+};
